df750ac60806d1d1d4fc32826fe081b03fe9c187
Raghu Betina <raghu@firstdraft.com>
2020-05-07 06:29:31 +0000

    Whitelist columns in Warehouse for ActiveAdmin

--- app/admin/warehouses.rb
+++ app/admin/warehouses.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Warehouse do
+
+ permit_params :location_name, :location
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
