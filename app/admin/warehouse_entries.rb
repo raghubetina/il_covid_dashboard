947bf9062115c186a0f8556d9079dd7b3c8ca811
Raghu Betina <raghu@firstdraft.com>
2020-05-07 06:29:41 +0000

    Whitelist columns in WarehouseEntry for ActiveAdmin

--- app/admin/warehouse_entries.rb
+++ app/admin/warehouse_entries.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register WarehouseEntry do
+
+ permit_params :number_anterior_swabs_received_24_hour, :number_anterior_swabs_sent_24_hour, :number_outstanding_orders_anterior_swabs, :current_inv_anterior_swabs, :number_nasopharyngeal_swabs_received_24_hour, :number_nasopharyngeal_swabs_sent_24_hour, :number_outstanding_orders_nasopharyngeal_swabs, :current_inv_nasopharyngeal_swabs, :number_transport_media_received_24_hour, :number_transport_media_sent_24_hour, :number_outstanding_orders_transport_media, :current_inv_transport_media, :warehouse_id, :for_24h_period_ending_at
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
