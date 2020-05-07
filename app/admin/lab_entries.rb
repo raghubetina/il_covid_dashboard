591fb952ffa1d539226f3b0b72ee0034e6fc8932
Raghu Betina <raghu@firstdraft.com>
2020-05-07 06:29:51 +0000

    Whitelist columns in LabEntry for ActiveAdmin

--- app/admin/lab_entries.rb
+++ app/admin/lab_entries.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register LabEntry do
+
+ permit_params :lab_id, :number_tests_arrived_24_hour, :number_tested_24_hour, :positive_rate_24_hour, :percent_capacity_achieved_24_hour, :average_hours_needed_to_process_tests_24_hour, :percent_of_tests_in_last_7_days_which_took_longer_than_24_hours, :total_number_tests_at_lab_which_require_processing, :for_24h_period_ending_at
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
