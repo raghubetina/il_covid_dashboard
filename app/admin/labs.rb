8255223f2fff30cbc1bbb97a66b8bed817bac231
Raghu Betina <raghu@firstdraft.com>
2020-05-07 06:29:37 +0000

    Whitelist columns in Lab for ActiveAdmin

--- app/admin/labs.rb
+++ app/admin/labs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Lab do
+
+ permit_params :location_name, :location
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
